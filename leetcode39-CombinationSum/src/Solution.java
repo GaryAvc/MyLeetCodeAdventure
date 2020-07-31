0b80a4058a6a063b8a7ef6c7e4aebca1038edacd
Gary Xu <xjs1997524@gmail.com>
2020-07-31 10:22:46 +0800

    [lc 39] 1. recursion, backtrack Still dont work it out , it is the record of my answer

---
+++ leetcode39-CombinationSum/src/Solution.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution s= new Solution();
+        int []can = {2,3,5};
+        int tar = 8;
+        System.out.println(s.combinationSum(can,8));
+    }
+
+    List<List<Integer>> answerList = new LinkedList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        bfs(0,target,candidates, new LinkedList<Integer>());
+        return answerList;
+    }
+
+    private void bfs(int sum, int target, int[]candidates
+            , List<Integer> candidateCombination){
+        if( sum > target){
+            return;
+        }
+
+        if( sum == target){
+            answerList.add(candidateCombination);
+            return;
+        }
+
+        for( int i :candidates){
+            candidateCombination.add(i);
+            bfs(sum+=i,target,candidates,candidateCombination);
+//            candidateCombination.remove(candidateCombination.size()-1);
+        }
+    }
+}
