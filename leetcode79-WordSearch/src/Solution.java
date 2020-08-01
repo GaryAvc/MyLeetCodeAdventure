5e95ccdf60a6e55cec448a28e9ba575e022e20f0
Gary Xu <xjs1997524@gmail.com>
2020-08-01 23:24:22 +0800

    [lc79] im am a trash

--- leetcode79-WordSearch/src/Solution.java
+++ leetcode79-WordSearch/src/Solution.java
@@ -1,43 +1,93 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
-    //用于标记是否已经找到了解
-    private boolean flag;
+
+    public static void main(String[] args) {
+
+//        [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+//        "ABCB"
+
+//        char [][] test = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+//        String wor ="ABCCED";
+//
+//        char [][] test1 = {{'a'}};
+//        String wor1 ="a";
+
+//        char [][] test2 = {{'a'},{'a'}};
+//        String wor2 ="aaa";
+
+        char [][] test = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String wor ="ABCB";
+        Solution s = new Solution();
+        s.exist(test,wor);
+    }
+
+    boolean success=false;
+    char[][] board;
+    char[] wordArray;
+    Set<String> duplicate = new HashSet<>();
+
 
     public boolean exist(char[][] board, String word) {
-        if(board == null || board.length == 0 || board[0].length == 0 ) {
+        this.board=board;
+        this.wordArray=word.toCharArray();
+
+        if(word == null||word==""){
             return false;
         }
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                if(dfs(board, i, j, word, 0)) {
+        if(word.length()==1 && !(wordArray[0]==(board[0][0]))){
+            return false;
+        }
+
+        for( int i=0;i< board.length;i++){
+            for (int j=0;j< board[i].length;j++){
+                if(dfs(wordArray.length-1,i,j)){
                     return true;
                 }
             }
         }
+
         return false;
     }
 
-    private boolean dfs(char[][] board, int i, int j, String word, int cur) {
-        if(cur == word.length()) {
-            flag = true;
-            return true;
-        }
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length
-                || board[i][j] != word.charAt(cur)) {
+    public boolean dfs( int remain, int x, int y){
+
+        char target = wordArray[wordArray.length-remain];
+
+        if( x<0|| y<0|| x>= board.length || y>= board[0].length){
             return false;
         }
-        //如果没有找到解，则继续DFS
-        if(!flag) {
-            char c = board[i][j];
-            board[i][j] = '.';
-            boolean ret1 = dfs(board, i + 1, j, word, cur + 1);
-            boolean ret2 = dfs(board, i - 1, j, word, cur + 1);
-            boolean ret3 = dfs(board, i, j + 1, word, cur + 1);
-            boolean ret4 = dfs(board, i, j - 1, word, cur + 1);
-            board[i][j] = c;
-            return ret1 || ret2 || ret3 || ret4;
-        }else {
-            //找到解了，直接结束DFS并返回，这就是剪枝
+
+        if(remain==1 && target == board[x][y] ){
+            success=true;
             return true;
         }
+
+        /*
+        this part check if the x,y cordinate is valid
+         */
+
+
+        if( duplicate.contains( x+" "+y)){
+            return false;
+        }
+        if(board[x][y]!=target){
+            return false;
+        }
+
+        if(!success){
+            duplicate.add(x+" "+y);
+            boolean rt1 = dfs(remain-1,x+1,y);
+            boolean rt2 = dfs(remain-1,x-1,y);
+            boolean rt3 = dfs(remain-1,x,y-1);
+            boolean rt4 = dfs(remain-1,x,y+1);
+            duplicate.remove(duplicate.size()-1);
+            return rt1||rt2||rt3||rt4;
+        }else{
+            return true;
+        }
+
     }
+
 }
