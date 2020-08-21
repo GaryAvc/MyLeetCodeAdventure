8f28748464b68e59d81b9d46592f315e96ab0a13
Gary Xu <xjs1997524@gmail.com>
2020-08-21 09:46:46 +0800

    < FAILED > [ lc 73 - 2 pointer ] 1.TODO - acoomplish Correct answer's idea

--- leetcode73-SetMatrixZeroes/src/Solution.java
+++ leetcode73-SetMatrixZeroes/src/Solution.java
@@ -2,49 +2,33 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int matrixRow = matrix.length;
         int matrixColumn = matrix[0].length;
+        boolean colZero =false;
 
 
         for( int i =0; i <matrixRow;++i){
             for( int j =0;j<matrixColumn;j++){
                 if( matrix[i][j] == 0){
-                    matrix = mark( matrix,i,j);
+                    if( j ==0){
+                        matrix[0][0]=0;
+                    }
+                    if( i ==0){
+                        colZero=true;
+                    }
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
 
         for( int i =0; i <matrixRow;++i){
             for( int j =0;j<matrixColumn;j++){
-                if( matrix[i][j] == Integer.MAX_VALUE){
-                    matrix[i][j]=0;
+                if( j ==0 && matrix[i][j]==0){
+                    matrix[i][]
                 }
             }
         }
 
     }
 
-    /*
-    this function take row, column and replace the item in it with MAX_Value
-    since the max value it can appear is 10^9 which is absolutely less than MAX_Value
-     */
-    private int[][] mark (int [][]matrix,int row,int column){
 
-        int matrixRow = matrix.length;
-        int matrixColumn = matrix[0].length;
-
-        for( int j =0;j<matrixColumn;j++){
-            if( matrix[row][j] == 0 && j!=column){
-                matrix = mark( matrix,row,j);
-            }
-            matrix[row][j] = Integer.MAX_VALUE;
-        }
-
-        for( int i =0; i <matrixRow;++i){
-            if( matrix[i][column] == 0 && i!=row){
-                matrix = mark( matrix,i,column);
-            }
-            matrix[i][column] = Integer.MAX_VALUE;
-        }
-
-        return matrix;
-    }
 }
---
+++ leetcode75-SortColors/src/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+
+//    int[]nums;
+
+    public void sortColors(int[] nums) {
+//        nums=nums;
+        int start = 0;
+        int end = nums.length-1;
+
+        while()
+
+    }
+
+//    private void sortColorsHelper ( int start,int end,int one,int ifOne){}
+}
