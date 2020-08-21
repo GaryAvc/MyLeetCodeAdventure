d232dc825883a955b13e1c673ccfad249e556179
Gary Xu <xjs1997524@gmail.com>
2020-08-21 10:34:43 +0800

    < SUCCESS > [ lc 73 - 2 pointer ] 1. implement the coreect idea 2. which the core part is 2 pointer 3. and make sure when it is swaping with the last item, do not make pointer ++, but only end -- , since it can be 2 swap with 2 and you need to swap this prev-swap-2 with the end-- position

--- leetcode75-SortColors/src/Solution.java
+++ leetcode75-SortColors/src/Solution.java
@@ -5,11 +5,51 @@ class Solution {
     public void sortColors(int[] nums) {
 //        nums=nums;
         int start = 0;
+        int pointer = 0;
         int end = nums.length-1;
+        int count =0;
+        int temp;
 
-        while()
+        if( nums.length <2){
+            return;
+        }
+
+        while( pointer<=end){
+            // if the start detect 2, swap it with end, end --, start NOT ++!
+            /*
+            the reason that when swap 2 with end, pointer NOT ++
+            but swap 0 with start, pointer ++, start ++
+            is that swap 2 contains one problem that if you swap 2 with 2, you still need to
+            swap this 2 to the end of the array
+            Since 0, start is at the same place , but 2, start is at different position
+             */
+            if( nums[pointer] == 2){
+                temp=nums[end];
+                nums[end] = 2;
+                nums[pointer]=temp;
+                end--;
+            }else if ( nums[pointer]== 0){
+                temp=nums[start];
+                nums[start] = 0;
+                nums[pointer] = temp;
+                pointer++;
+                start++;
+            }else{
+                pointer++;
+            }
+        }
 
     }
+
+
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int [] x ={2,0,2,1,1,0};
+        s.sortColors(x);
+    }
+
 
 //    private void sortColorsHelper ( int start,int end,int one,int ifOne){}
 }
