40e41d0d507c17d0fa82d4d2d025289768ae94e1
Gary Xu <xjs1997524@gmail.com>
2020-08-24 10:58:05 +0800

    < SUCCESS > [ lc 91 - DP ] 1. set a array of n+1 is really suit for n-1, n-2 model 2. initial the index 0 to 1 can initial the problem 3. set index 1 to ? '0' 0:1 can nicely prevent the situation when 0 is in the front and 09 can nerver reach the statement of seconde 10 ~26, so it will also not add dp[i-2] which is 1

--- leetcode91-DecodeWays/src/Solution.java
+++ leetcode91-DecodeWays/src/Solution.java
@@ -1,50 +1,34 @@
 class Solution {
     public int numDecodings(String s) {
 
-        char[] values = s.toCharArray();
+        int n = s.length();
+        int [] dp = new int[n+1];
 
-        return -1;
-    }
-
-    /*
-    Invirance:
-        1. when the value is 1 or 2, it will cause special case
-        2. when it is 1 : it can attach 0~9
-        3. when it is 2 : it can only attach 0~6 ( include 6 )
-        4. when 1,2 is going to recursion into 1,2 DO NOT add 1, only when it is going recursion into
-            0,3,4,5,6,7,8,9, then add 1 into the answer
-        5. when decide to run it into a 2 digits answer, add 1 into the answer
-        6. only if when it is the last digit, do NOT consider the 1,2 possible
-     */
-    private int dp ( char[] values, int index, int ways ){
-
-        // when reach the end of the array
-        if( index == values.length){
+        if( s == null || s.length()==0){
             return 0;
         }
 
-        // 6. make sure that it is not the last digit
-        if( index != values.length -1 ) {
-            if (values[index] == '1') {
-                if( values[index+1] =='1' || values[index+1] =='2'){
-                    ways++;
-                    dp(values,index+1,ways);
-                    dp(values,index+2,ways);
-                }else{
-                    ways++;
+//        if( n ==1 && Integer.parseInt( s.substring(0,1))==0){
+//            return 0;
+//        }
 
-                }
-            } else if( values[index] == '2'){
-                if( values[index+1] =='1' || values[index+1] =='2'){
-                    ways++;
-                    dp(values,index+1,ways);
-                }else{
-                    ways++;
-                }
+        dp[0]=1;
+        dp[1] = s.charAt(0) == '0' ? 0:1;
+
+        for( int i =2;i<=n;i++){
+            int first = Integer.parseInt( s.substring( i-1,i));
+            int second = Integer.parseInt( s.substring(i-2,i));
+
+            if( first >=1 && first <=9){
+                dp[i] += dp[i-1];
+            }
+            if( second>=10 && second <=26){
+                dp[i]+=dp[i-2];
             }
         }
+        return dp[n];
 
-        return -1;
+
     }
 
 }
