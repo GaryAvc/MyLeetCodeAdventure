4bea71770eb271b6d2347e1a511500575d55dde3
Gary Xu <xjs1997524@gmail.com>
2020-09-04 09:56:10 +0800

    < Failed  >[ lc-146 ] 1. GET operation should (1) delete the original index in age (2) add a new Index into the age to -renew- this index, Otherwise it will be polled as the get operation not happening before

--- leetcode139-WordBreak/src/Solution.java
+++ leetcode139-WordBreak/src/Solution.java
@@ -1,100 +1,34 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
 
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        List<String> x =new LinkedList<>();
-        x.add("cats");
-        x.add("dog");
-        x.add("cat");
-        x.add("ski");
-
-        System.out.println(s.wordBreak("catskicatcats",x));
-    }
-
-    // this map is a map with key: word's first char in wordDict, value: the char[] of word in dictionary
-    Map<Character, List<char[]>> firstLetter;
-    boolean answer;
-    Set<Character> dictionary;
-
+    /*
+    Invariance:
+    1. use a array to store if this index is cutable
+    2. the subString of s.substring(j,i) is sutting the first char to the whole String as i grows. push one start letter later as j grows.
+     */
     public boolean wordBreak(String s, List<String> wordDict) {
 
-        /*
-        Invariance:
-        1. if string s contain a char that not exist in wordDict, just return false;
-         */
-        dictionary=new HashSet<>();
-        answer = false;
-        firstLetter= new HashMap<>();
-        for( int i=0;i<wordDict.size();++i){
+        Set<String> dictionary = new HashSet<>(wordDict);
 
-            char[] word =wordDict.get(i).toCharArray();
+        boolean [] correct = new boolean[s.length()+1];
+        correct[0]=true;
 
-            for(int j =0;j<word.length;++j){
-                if(!dictionary.contains(word[j])){
-                    dictionary.add(word[j]);
+        for( int i =1;i<=s.length();++i){
+            for( int j=0;j<i;j++){
+
+                if( correct[j] && dictionary.contains(s.substring(j,i))){
+                    correct[i]=true;
+                    break;
                 }
-            }
-            char first = word[0];
 
-            if( firstLetter.containsKey(first)){
-                firstLetter.get(first).add(word);
-            }else{
-                List<char[]> words = new LinkedList<>();
-                words.add(word);
-                firstLetter.put(first,words );
-            }
-
-        }
-
-        char[] targetChar = s.toCharArray();
-        for( int i =0;i<targetChar.length;++i){
-            if(!dictionary.contains(targetChar[i])){
-                return answer;
             }
         }
 
-        helper(s);
-        return answer;
-
-
+        return correct[s.length()];
     }
 
-    // recurive to solve the prob.
-    private boolean helper(String s){
 
-        //basecase: if s is empty, return true
-        if(s.length()==0){
-            answer=true;
-            return true;
-        }
-
-        char[] target =s.toCharArray();
-        // base case: if the word input, does not contain the first char in the map, it can not be a solution
-        if( !firstLetter.containsKey(target[0]) ){
-            return false ;
-        }
-
-        List<char[]> words = firstLetter.get(target[0]);
-
-        // iterate the possible dictionary words char[], to see if there exist dictionary words match all char in target char array
-        outerLoop:
-        for(int i =0;i<words.size();++i){
-            char [] word = words.get(i);
-            if(target.length<word.length){
-                continue;
-            }
-            for( int j=0;j<word.length;j++){
-                if( target[j]!=word[j] ){
-                    continue outerLoop;
-                }
-            }
-            if(helper(s.substring(word.length, target.length))){
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
---
+++ leetcode146-LRUCache/src/Solution.java
@@ -0,0 +1,128 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+
+class LRUCache {
+
+    public static void main(String[] args) {
+        LRUCache s = new LRUCache(10);
+        String[] order = {"put","put","put","put","put","get","put","get","get","put","get",
+                "put","put","put","get","put","get","get","get","get","put","put",
+                "get","get","get","put","put","get","put","get","put","get","get",
+                "get","put","put","put","get","put","get","get","put","put","get",
+                "put","put","put","put","get","put","put","get","put","put","get",
+                "put","put","put","put","put","get","put","put","get","put","get",
+                "get","get","put","get","get","put","put","put","put","get","put",
+                "put","put","put","get","get","get","put","put","put","get","put",
+                "put","put","get","put",
+                "put","put","get","get","get","put","put","put","put","get","put",
+                "put","put","put","put","put","put"};
+        List<int[]> lists = new ArrayList<>();
+
+       try{
+           File myObj = new File("/Users/mac/WorkSpace/LeetCodeWorkSpace/leetcode138-CopyListwithRandomPointer/src/nums.txt");
+           Scanner myReader = new Scanner(myObj);
+           int count =0;
+           while (myReader.hasNextLine()) {
+               String data = myReader.nextLine();
+               if(data.contains(",")){
+                   System.out.print("PUT ");
+
+                   int splitIndex = data.indexOf(',');
+                   String first = data.substring(0,splitIndex);
+                   String second = data.substring(splitIndex+1);
+                   int fir = Integer.parseInt(first);
+                   int sec = Integer.parseInt(second);
+
+                   System.out.print(fir);
+                   System.out.print(" " + sec + " null \n");
+                   s.put(fir,sec);
+               }else{
+                   System.out.print("GET " + data + " "+ s.get(Integer.parseInt(data)) + "\n");
+               }
+               count++;
+//               System.out.println(data);
+           }
+           myReader.close();
+       }catch (FileNotFoundException e) {
+           System.out.println("An error occurred.");
+           e.printStackTrace();
+       }
+
+
+    }
+
+
+    /*
+    Invariance:
+    1. Stack can be a good data structure to keep update the age of all process
+    2. A map should be used to maintain the key and value
+     */
+
+    Queue<Integer> age;
+    Map<Integer,Integer> map;
+    int size;
+
+    public LRUCache(int capacity) {
+        age=new ArrayDeque<>();
+        map = new HashMap<>();
+        size=capacity;
+    }
+
+    /*
+    Invariance:
+    1. get should renew the age of all items
+     */
+    public int get(int key) {
+
+        // check if the key exists, if yes, return index, if not, return -1
+        if(map.containsKey(key)){
+
+            // update age
+            renew(key,false);
+
+            // return value
+            return map.get(key);
+        }else{
+            return -1;
+        }
+
+
+    }
+
+    public void put(int key, int value) {
+
+        if( map.containsKey(key)){
+            map.replace(key,value);
+            renew(key,false);
+        }else {
+            renew(key , true);
+            map.put(key,value);
+
+        }
+    }
+
+    // if putOperation: renew and remove
+    // if not putOperation: only renew the order
+
+    private void renew(int key,boolean putOperation) {
+
+        // renew the age if the key exists
+
+
+
+        if(age.size()>=size){
+            int removedKey = age.poll();
+            age.add(key);
+            if(putOperation){
+                map.remove(removedKey);
+            }
+        }else{
+            if(!age.contains(key)){
+                age.add(key);
+            }
+        }
+
+    }
+}
