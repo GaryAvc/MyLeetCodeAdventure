922277afa85ddc5089c7d213092cd318cdc39c2e
Gary Xu <xjs1997524@gmail.com>
2021-01-26 19:59:00 +0800

    < Finished - but not so many time improved >[ lc -9 ] 1. use only half but get slower

--- leetcode9-PalindromeNumber/src/Solution.java
+++ leetcode9-PalindromeNumber/src/Solution.java
@@ -2,11 +2,30 @@ class Solution {
 
     public boolean isPalindrome(int x) {
 
+        int initialX= x;
+
         if( x < 0 ){
             return false;
         }else{
 
-            if( x == backNum(x)){
+            int answer = 0;
+            StringBuilder sb = new StringBuilder();
+            sb.append(x);
+            int length = sb.toString().length();
+            int n = sb.toString().length() / 2;
+
+            while( n!=0){
+
+                int digit = x%10;
+                x=x/10;
+                n--;
+
+                answer = answer *10 + digit;
+            }
+
+            if( x == answer && length %2 ==0){
+                return true;
+            }else if(x/10 == answer && length%2==1) {
                 return true;
             }else{
                 return false;
@@ -16,18 +35,5 @@ class Solution {
 
     }
 
-    public int backNum(int x){
 
-        int answer = 0;
-
-        while( x!=0){
-
-            int digit = x%10;
-            x=x/10;
-
-            answer = answer *10 + digit;
-        }
-
-        return  answer;
-    }
 }
