ac2fee5fa15840196cd9ccb1b194baa045bce2ad
Gary Xu <xjs1997524@gmail.com>
2021-02-04 18:47:44 +0800

    < 2 time - FINISHED >[ lc - 101 ] 1. finished use recursion

--- leetcode101-SymmetricTree/src/Solution.java
+++ leetcode101-SymmetricTree/src/Solution.java
@@ -15,38 +15,68 @@ class TreeNode {
 
 public class Solution {
 
-    ArrayList list = new ArrayList();
-    public boolean isSymmetric(TreeNode root) {
+    // --- iteratively ---
 
 
 
-        inorder(root);
+    // -- end of iteratively --
 
-        for( int i =0;i<list.size();i++){
-            if( list.get(i) != list.get(list.size()-1-i)){
-                return false;
-            }
-        }
-        return true;
-    }
+//    // ---- recursion ----
+//    public boolean isSymmetric(TreeNode root) {
+//
+//        TreeNode l = root.left;
+//        TreeNode r = root.right;
+//
+//        return check(l,r);
+//
+//    }
+//
+//    public boolean check( TreeNode l, TreeNode r){
+//
+//        if(l==null && r==null){
+//            return true;
+//        }else if(l==null || r==null){
+//            return false;
+//        }else{
+//            return (l.val==r.val) && check(l.left,r.right) &&check(l.right,r.left);
+//        }
+//    }
+//
+//    // ---- end of recursion ----
 
-    public void inorder(TreeNode root){
 
-        if( root ==null){
-            list.add(-1);
-            return;
-        }
-        if((root.left== null && root.right!=null)){
-            list.add(-1);
-        }
-        inorder(root.left);
-        list.add(root.val);
-        if((root.right == null && root.left!=null) ){
-            if(root.right == null) {
-                list.add(-1);
-            }
-        }
-        inorder(root.right);
+//    ArrayList list = new ArrayList();
+//    public boolean isSymmetric(TreeNode root) {
+//
+//
+//
+//        inorder(root);
+//
+//        for( int i =0;i<list.size();i++){
+//            if( list.get(i) != list.get(list.size()-1-i)){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 
-    }
+//    public void inorder(TreeNode root){
+//
+//        if( root ==null){
+//            list.add(-1);
+//            return;
+//        }
+//        if((root.left== null && root.right!=null)){
+//            list.add(-1);
+//        }
+//        inorder(root.left);
+//        list.add(root.val);
+//        if((root.right == null && root.left!=null) ){
+//            if(root.right == null) {
+//                list.add(-1);
+//            }
+//        }
+//        inorder(root.right);
+//
+//    }
 }
